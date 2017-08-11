01aa9a6dd5bb86f572872d80d85350fa4a668958
manhaam <fldksfpem@naver.com>
2017-08-11 09:37:03 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title> New Document </title>
+  <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+  <meta name="Generator" content="NPP-Plugin">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+ </head>
+ <style></style>
+ <!--<script type="text/javascript">
+  </script>-->
+ <body>
+ <h1>Hello World!</h1>
+ <?php ?>
+  
+ </body>
+</html>
